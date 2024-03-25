93732f0967787cee86f3262936e342f2009d07e0
uginuss <ztripolka@gmail.com>
2024-03-25 17:27:41 +0100

    modified:   index.html 	modified:   script.js 	modified:   style.css

--- script.js
+++ script.js
@@ -19,6 +19,34 @@ xhr.onload=()=>{
     }
 };
 
+const jokeform=document.getElementById('joke_form');
+jokeform.addEventListener('submit', (event)=>{
+
+
+
+    const addjokerxhr=new XMLHttpRequest();
+    addjokerxhr.open('POST', 'http://localhost:3000/addjoke');
+    let inpform=document.getElementById('joke_form_input');
+    let joketosend={
+        id: `${currentlength}`,
+        content: `${inpform.value}`,
+        likes: 0,
+        dislikes: 0,
+        needSHOW: true
+    }
+    if(inpform.value!=''){
+        addjokerxhr.send(JSON.stringify(joketosend));
+    }else{
+        console.log('debug-mod');
+    }
+});
+
+
+
+
+
+
+
 
 function getjokeshtml(joke){
     return `
@@ -29,7 +57,7 @@ function getjokeshtml(joke){
         <div class="joke__footer">
             <div class="joke__likes joke_like">
                 <span>${joke.likes}</span>
-                <button class="joke__btn">
+                <button class="joke__btn" oncliock=''>
                     <span class="material-symbols-outlined">
                         thumb_up
                     </span>
